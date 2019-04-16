4442ba38f57dc65dcdd3dfb9e2eedd1946987d7b
Matt Scheetz <mfscheetz@gmail.com>
2019-04-16 19:43:09 -0400

    Setting up more ETH stuff

--- chain-hunter/src/app/classes/Connections.ts
+++ chain-hunter/src/app/classes/Connections.ts
@@ -3,5 +3,5 @@ export class Connections{
     constructor(){}
 
     btcBase: string = "https://chain.api.btc.com/v3";
-    ethBase: string = "";
+    ethBase: string = "https://mainnet.infura.io/v3/chainhunter";
 }
---
+++ chain-hunter/src/app/services/eth-svc.service.ts
@@ -0,0 +1,58 @@
+import { Connections } from '../classes/Connections';
+import { Observable } from 'rxjs';
+import { BtcAddress } from '../classes/BTC/BtcAddress';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BtcTransaction } from '../classes/BTC/BtcTransaction';
+import { Injectable } from '@angular/core';
+import { BtcBase } from '../classes/BTC/BtcBase';
+import { BtcPage } from '../classes/BTC/BtcPage';
+import { delay } from 'rxjs/operators';
+
+@Injectable({providedIn: 'root'})
+export class EthService{
+    constructor(private http: HttpClient) {}
+
+    conn: Connections = new Connections();
+    base: string = this.conn.btcBase;
+
+    /**
+     * Get a BTC address
+     * 
+     * @param address Address to check
+     */
+    getAddress(address: string): Observable<BtcBase<BtcAddress>>{
+        let endpoint: string = "/address/" + address;
+        let url: string = this.base + endpoint;
+
+        let result = this.http.get<BtcBase<BtcAddress>>(url)
+        .pipe(delay(1000));
+    
+        return result;
+    }
+
+    /**
+     * Get Transactions for a BTC Address
+     * 
+     * @param address Address to check
+     */
+    getAddressTransactions(address: string): Observable<BtcBase<BtcPage<BtcTransaction[]>>>{
+        let endpoint: string = "/address/" + address +"/tx";
+        let url: string = this.base + endpoint;
+
+        return this.http.get<BtcBase<BtcPage<BtcTransaction[]>>>(url)
+        .pipe(delay(1000));
+    }
+
+    /**
+     * Get a BTC transaction
+     * 
+     * @param transaction Transaction to check
+     */
+    getTransaction(transaction: string): Observable<BtcBase<BtcTransaction>>{
+        let endpoint: string = "/tx/" + transaction + "?verbose=3";
+        let url: string = this.base + endpoint;
+
+        return this.http.get<BtcBase<BtcTransaction>>(url)
+        .pipe(delay(1000));
+    }
+}

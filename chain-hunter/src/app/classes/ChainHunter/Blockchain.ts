55d6109a9d7eaf43b8f21106092de71abdadb643
Scheetz <mfscheetz@gmail.com>
2019-05-28 16:09:32 -0400

    Update Blockchain.ts

--- chain-hunter/src/app/classes/ChainHunter/Blockchain.ts
+++ chain-hunter/src/app/classes/ChainHunter/Blockchain.ts
@@ -1,6 +1,14 @@
 export class Blockchain {
     constructor () {}
 
+    constructor(name: string, symbol:string) {
+	this.Name = name;
+	this.Symbol = symbol;
+	this.Found = false;
+	this.Address = null;
+	this.Transaction = null;
+    }
+
     Name: string;
     Symbol: string;
     Found: boolean;

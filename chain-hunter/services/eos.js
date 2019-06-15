62278bd7a565a3fc5ec339a7ff3b8b568957a5de
Matt Scheetz <mfscheetz@gmail.com>
2019-06-15 15:29:55 -0400

    starting on eos

---
+++ chain-hunter/services/eos.js
@@ -0,0 +1,108 @@
+const axios = require('axios');
+const helperSvc = requre('helper');
+const base = "https://eos.greymass.com/v1";
+
+const getBlockchain = function(toFind) {
+    const chain = {};
+    chain.name = 'EOS.IO';
+    chain.symbol = 'EOS';
+    chain.hasTokens = true;
+
+    const address = await getAddress(toFind);
+    chain.address = address;
+    chain.transaction = null;
+    if(address === null) {
+        const transaction = await getTransaction(toFind);
+        chain.transaction = transaction;
+    }
+
+    return chain;
+}
+
+const getAddress = async(address) => {
+    let endpoint = "/chain/get_currency_balance";
+    let url = base + endpoint;
+    let data = '{"code":"eosio.token","account":"'+ address +'","symbol":"EOS"}';
+
+    try{
+        const response = await axios.post(url, data);
+        if(response.length > 0) {
+            const address = {
+                address:  address,
+                balance: parseFloat(response[0].replace(/\D/g, "")),
+            };
+
+            return address;
+        } else {
+            return null;
+        }
+    } catch(error) {
+        return null;
+    }
+}
+
+const getTransactions = async(address) => {
+    let endpoint = "/history/get_actions";
+    let url = base + endpoint;
+    let data = '{"pos":"-1","offset":"-10","account_name":"'+ address +'"}';
+
+    try{
+        const response = await axios.get(url);
+        if(response.err_no === 0 && response.data !== null) {
+            const datas = response.data.list;
+            const transactions = [];
+            if(datas.length > 0) {
+                datas.forEach(data => {
+                    transactions.push(buildTransaction(data));
+                })
+            }
+
+            return transactions;
+        } else {
+            return [];
+        }
+    } catch(error) {
+        return [];
+    }
+}
+
+const getTransaction = function(hash) {
+    let endpoint = "/tx/" + hash + "?verbose=3";
+    let url = base + endpoint;
+
+    try{
+        const response = await axios.get(url);
+        if(response.err_no === 0 && response.data !== null) {
+            const data = response.data;
+            const transaction = buildTransaction(data);
+
+            return transaction;
+        } else {
+            return null;
+        }
+    } catch(error) {
+        return null;
+    }
+}
+
+const buildTransaction = function(txn) {
+    const transaction = {
+    hash: txn.hash,
+    block: txn.block_height,
+    quantity: txn.outputs_value/100000000,
+    symbol: "BCH",
+    confirmations: txn.confirmations,
+    date: helperSvc.unixToUTC(txn.created_at),
+    from: txn.inputs[0].prev_addresses[0],
+    to: txn.outputs[0].addresses[0]
+    };
+
+    return transaction;
+}
+
+module.exports = {
+    getBlockchain,
+    getAddress,
+    getTransactions,
+    getTransaction
+}

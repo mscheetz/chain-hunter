84e3261a772041a37ff45810498a86f9516a565c
Matt Scheetz <mfscheetz@gmail.com>
2019-07-08 16:57:32 -0700

    adding cookies component

---
+++ src/app/components/cookies/cookies.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'cookies',
+    templateUrl: './cookies.component.html',
+    styleUrls: ['./cookies.component.css']
+})
+
+export class CookiesComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}

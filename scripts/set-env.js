783a1ed2804d778af65b01726a344cf157d7ffa8
Matt Scheetz <mfscheetz@gmail.com>
2019-08-06 21:03:58 -0400

    utilize environment variables

---
+++ scripts/set-env.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const yargs = require('yargs');
+
+// This is good for local dev environments, when it's better to
+// store a projects environment variables in a .gitignore'd file
+require('dotenv').config();
+
+// Would be passed to script like this:
+// `ts-node set-env.ts --environment=dev`
+// we get it from yargs's argv object
+const environment = yargs.argv.environment;
+const isProd = environment === 'prod';
+
+const targetPath = environment === 'dev' 
+    ? `./src/environments/environment.ts`
+    : `./src/environments/environment.${environment}.ts`;
+const envConfigFile = `
+export const environment = {
+  production: ${isProd},
+  user: "${process.env.CHAINHUNTER_USER}",
+  token: "${process.env.CHAINHUNTER_TOKEN}" 
+};
+`
+fs.writeFile(targetPath, envConfigFile, function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log(`Output generated at ${targetPath}`);
+});

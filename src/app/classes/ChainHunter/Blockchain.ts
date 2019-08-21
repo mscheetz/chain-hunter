daec7f22515146e584936290c8b4807c1da5e0d4
Matt Scheetz <mfscheetz@gmail.com>
2019-08-20 20:07:06 -0400

    updated footer

--- src/app/classes/ChainHunter/Blockchain.ts
+++ src/app/classes/ChainHunter/Blockchain.ts
@@ -12,6 +12,7 @@ export class Blockchain {
     transaction: Transaction;
     contract: Contract;
     icon: string;
+    type: string;
     source: string = null;
     hasTokens: boolean = false;
     hasContracts: boolean = false;

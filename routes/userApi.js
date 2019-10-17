28c215498c97d9ecb0b6189fd6538ff73ba7b7ee
Matt Scheetz <mfscheetz@gmail.com>
2019-10-16 20:26:03 -0400

    merge login to user, use generic responses

--- routes/userApi.js
+++ routes/userApi.js
@@ -2,19 +2,61 @@ const express = require("express");
 const router = express.Router();
 const userSvc = require('../services/userService');
 const apiHelp = require("../services/apiHelper");
+const encryptionSvc = require('../services/encryption');
+
+router.get("/api/user/login", apiHelp.asyncMiddleware(async (req, res, next) => {
+  res.status(200).json("howdy folks!");
+}));
+
+router.post(
+  "/api/user/login",
+  apiHelp.asyncMiddleware(async (req, res, next) => {
+    const headerMsg = apiHelp.headerCheck(req);
+    if (!headerMsg.status) {
+      apiHelp.errorResponse(res);
+    } else {
+      const username = req.body.username,
+        password = req.body.password;
+
+        const result = await userSvc.login(username, password);
+
+        res.status(result.code).json(result.data);
+    }
+  })
+);
+
+
+router.post(
+  "/api/user/login/shucks",
+  apiHelp.asyncMiddleware(async (req, res, next) => {
+    const headerMsg = apiHelp.headerCheck(req);
+    if (!headerMsg.status) {
+      apiHelp.errorResponse(res);
+    } else {
+      const password = req.body.password;
+
+      const result = await encryptionSvc.hashPassword(password);
+
+      res.status(200).json(result);
+    }
+  })
+);
+
+router.get("/api/logout", apiHelp.asyncMiddleware(async (req, res, next) => {}));
 
 router.get(
   "/api/user/:userId",
   apiHelp.asyncMiddleware(async (req, res, next) => {
+    encryptionSvc.validateToken(req, res, next);
     const headerMsg = apiHelp.headerCheck(req);
     if (!headerMsg.status) {
       apiHelp.errorResponse(res);
     } else {
       const userId = req.params.userId;
 
-      const results = await userSvc.getUserByUserId(userId);
+      const result = await userSvc.getUserByUserId(userId);
 
-      res.status(200).json(results);
+      res.status(result.code).json(result.data);
     }
   })
 )
@@ -31,9 +73,9 @@ router.post(
     } else {
       const data = req.body.data;
 
-      const results = await userSvc.registerUser(data);
+      const result = await userSvc.registerUser(data);
 
-      res.status(200).json(results);
+      res.status(result.code).json(result.data);
     }
   })
 );
@@ -50,9 +92,9 @@ router.put(
     } else {
       const data = req.body.data;
 
-      const results = await userSvc.updateUser(data);
+      const result = await userSvc.updateUser(data);
 
-      res.status(200).json(results);
+      res.status(result.code).json(result.data);
     }
   })
 );
@@ -69,9 +111,9 @@ router.post(
     } else {
       const data = req.body.data;
 
-      const results = await userSvc.changePassword(data.userId, data.oldPassword, data.newPassword);
+      const result = await userSvc.changePassword(data.userId, data.oldPassword, data.newPassword);
 
-      res.status(200).json(results);
+      res.status(result.code).json(result.data);
     }
   })
 );
@@ -88,9 +130,9 @@ router.get(
     } else {
       const userId = req.params.userId;
 
-      const results = await userSvc.getUserData(userId);
+      const result = await userSvc.getUserData(userId);
 
-      res.status(200).json(results);
+      res.status(result.code).json(result.data);
     }
   })
 );
@@ -107,9 +149,9 @@ router.post(
     } else {
       const data = req.body.data;
 
-      const results = await userSvc.addUserData(data.userId, data.hash, data.symbol, data.type);
+      const result = await userSvc.addUserData(data.userId, data.hash, data.symbol, data.type);
 
-      res.status(200).json(results);
+      res.status(result.code).json(result.data);
     }
   })
 );
@@ -126,9 +168,9 @@ router.delete(
     } else {
       const data = req.body.data;
 
-      const results = await userSvc.deleteUserData(data.userId, data.hash, data.symbol, data.type);
+      const result = await userSvc.deleteUserData(data.userId, data.hash, data.symbol, data.type);
 
-      res.status(200).json(results);
+      res.status(result.code).json(result.data);
     }
   })
 );

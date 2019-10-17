bce0e27e73c0c8ca805d09bf5f5e75da0259159c
Matt Scheetz <mfscheetz@gmail.com>
2019-10-16 20:25:22 -0400

    use generic responses

--- services/userService.js
+++ services/userService.js
@@ -1,6 +1,31 @@
 const db = require('../data/dataRepo');
 const encryptionSvc = require("./encryption.js");
 const helperSvc = require('./helperService');
+const apiHelp = require('../services/apiHelper');
+
+/**
+ * Login to account
+ * 
+ * @param {string} username username
+ * @param {string} password password
+ */
+const login = async(username, password) => {
+    let user = username.indexOf('@') > 0 
+                ? await db.getUserByEmail(username)
+                : await db.getUser(username);
+
+    const validLogin = await encryptionSvc.checkPassword(password, user.password);
+
+    if(validLogin) {
+        const token = await encryptionSvc.getToken(user);
+        user.token = token;
+        delete user.password;
+
+        return apiHelp.successMessage(user);
+    } else {
+        return apiHelp.errorMessage("Invalid login");
+    }
+}
 
 /**
  * Register a user
@@ -12,7 +37,7 @@ const registerUser = async(user) => {
 
     const status = await db.postUser(user);
 
-    return status;
+    return apiHelp.successMessage(status, 201);
 }
 
 /**
@@ -22,7 +47,7 @@ const registerUser = async(user) => {
 const updateUser = async(user) => {    
     const status = await db.updateUser(user);
 
-    return status;
+    return apiHelp.successMessage(status, 202);
 }
 
 /**
@@ -37,14 +62,14 @@ const changePassword = async(userId, oldPassword, newPassword) => {
     const validPwd = encryptionSvc.checkPassword(oldPassword, user.hash);
 
     if(!validPwd) {
-        return "invalid password";
+        return apiHelp.errorMessage("Invalid password", 400);
     }
 
     const newHash = await encryptionSvc.hashPassword(newPassword);
 
     const status = await db.updateUserPassword(userId, user.hash, newHash);
 
-    return status;
+    return apiHelp.successMessage(status, 202);
 }
 
 /**
@@ -53,7 +78,9 @@ const changePassword = async(userId, oldPassword, newPassword) => {
  * @param {string} username 
  */
 const getUser = async(username) => {
-    return await db.getUser(username);
+    const user = await db.getUser(username);
+
+    return apiHelp.successMessage(user[0]);
 }
 
 /**
@@ -62,7 +89,9 @@ const getUser = async(username) => {
  * @param {string} userId 
  */
 const getUserByUserId = async(userId) => {
-    return await db.getUserByUserId(userId);
+    const user = await db.getUserByUserId(userId);
+    
+    return apiHelp.successMessage(user[0]);
 }
 
 /**
@@ -71,7 +100,9 @@ const getUserByUserId = async(userId) => {
  * @param {string} userId user id
  */
 const getUserData = async(userId) => {
-    return await db.getUserData(userId);
+    const data = await db.getUserData(userId);
+    
+    return apiHelp.successMessage(data[0]);
 }
 
 /**
@@ -93,7 +124,9 @@ const addUserData = async(userId, hash, chain, type) => {
         type: type,
         added: created
     }
-    return await db.postUserData(userData);
+    const result = await db.postUserData(userData);
+        
+    return apiHelp.successMessage(result, 201);
 }
 /**
  * Delete user saved search
@@ -101,10 +134,13 @@ const addUserData = async(userId, hash, chain, type) => {
  * @param {string} id saved search id
  */
 const deleteUserData = async(id) => {
-    return await db.deleteUserData(id);
+    const result = await db.deleteUserData(id);
+        
+    return apiHelp.successMessage(result, 202);
 }
 
 module.exports = {
+    login,
     registerUser,
     updateUser,
     changePassword,

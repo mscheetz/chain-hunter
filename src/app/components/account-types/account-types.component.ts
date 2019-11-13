48f73d6387c39585dccbbad64df8682a35360446
Matt Scheetz <mfscheetz@gmail.com>
2019-11-12 21:21:50 -0500

    formatting account types

--- src/app/components/account-types/account-types.component.ts
+++ src/app/components/account-types/account-types.component.ts
@@ -3,6 +3,8 @@ import { ApiService } from 'src/app/services/api.service';
 import { AccountType } from 'src/app/classes/AccountType';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { User } from 'src/app/classes/User';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-account-types',
@@ -14,7 +16,7 @@ export class AccountTypesComponent implements OnInit {
   loggedIn: boolean;
   user: User;
 
-  constructor(private apiSvc: ApiService, private authSvc: AuthenticationService) { 
+  constructor(private apiSvc: ApiService, private authSvc: AuthenticationService, private router: Router, private loginSvc: LoginService) { 
     this.apiSvc.getAccountTypes()
       .subscribe(res => {
         this.accounts = res;
@@ -28,9 +30,11 @@ export class AccountTypesComponent implements OnInit {
 
   onOrder(accountTypeId: number) {
     const hi = true;
+    this.loginSvc.toggleLogin();
   }
   
   onUpgrade(accountTypeId: number) {
     const hi = true;
+    this.router.navigate(['payment']);
   }
 }

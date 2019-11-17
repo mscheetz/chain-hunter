c7e3aa14a8c812aa41817d7416b744fe0f0a64dc
Matt Scheetz <mfscheetz@gmail.com>
2019-11-16 21:09:08 -0500

    setting up payment ss

--- services/payment.service.js
+++ services/payment.service.js
@@ -1,6 +1,12 @@
 const SquareConnect = require('square-connect');
 const config = require('../config');
 
+const squareClient = SquareConnect.ApiClient.instance;
+const oauth2 = squareClient.authentications['oauth2'];
+oauth2.accessToken = config.SQUARE_TOKEN;
+
+squareClient.basePath = config.ENV === 'DEV' ? 'https://connect.squareupsandbox.com' : 'https://connect.squareup.com';
+
 const processPayment = async() => {
 
 }

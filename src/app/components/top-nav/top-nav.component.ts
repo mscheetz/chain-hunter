61095ec54429cf9868c6156477ebccd8769bc2d0
Matt Scheetz <mfscheetz@gmail.com>
2019-11-23 13:04:20 -0500

    responsive navigation

--- src/app/components/top-nav/top-nav.component.ts
+++ src/app/components/top-nav/top-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { LoginService } from 'src/app/services/login.service';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-top-nav',
@@ -12,21 +13,45 @@ export class TopNavComponent implements OnInit {
   loggedIn: boolean;
   //@Output() loginSuccess: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  constructor(private loginSvc: LoginService, private authSvc: AuthenticationService) { 
+  constructor(private loginSvc: LoginService, 
+              private authSvc: AuthenticationService,
+              private router: Router) { 
     this.authSvc.isLoggedIn.subscribe(val => this.loggedIn = val);
   }
 
   ngOnInit() {
   }
 
+  goTo(route: string) {
+    route = `/${route}`;
+    this.hideNav();
+    this.router.navigate([route]);
+  }
+
   login(event) {
     console.log("Login toggle from top-nav.component");
     this.loginSvc.toggleLogin();
+    this.hideNav();
     //this.toggleLogin.emit(event);
   }
 
   logout(event) {
     this.authSvc.logout();
+    this.hideNav();
     //this.loginSuccess.emit(false);
+  }
+
+  navExpand() {
+    const nav = document.getElementById("topNav");
+    if(nav.className === "top-nav") {
+      nav.className += " responsive";
+    } else {
+      nav.className = "top-nav";
+    }
+  }
+
+  hideNav() {
+    const nav = document.getElementById("topNav");
+    nav.className = "top-nav";
   }
 }

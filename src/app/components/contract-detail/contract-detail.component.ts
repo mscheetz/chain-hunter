a95cb2040c94fc866cf8c5616d5758d24d8400d8
Matt Scheetz <mfscheetz@gmail.com>
2019-11-29 13:11:39 -0500

    fixed contract saves

--- src/app/components/contract-detail/contract-detail.component.ts
+++ src/app/components/contract-detail/contract-detail.component.ts
@@ -31,7 +31,12 @@ export class ContractDetailComponent implements OnInit {
 
   saveHover(event, type: string, overlayPanel: OverlayPanel) {
       this.saveThisMessage = "Save this " + this.blockchain.symbol + " " + type;
-    //this.saveThisMessage = "Coming Soon! Save this " + this.blockchain.symbol + " " + type;
+    
+    overlayPanel.toggle(event);
+  }
+
+  unSaveHover(event, type: string, overlayPanel: OverlayPanel) {
+      this.saveThisMessage = "Un-save this " + this.blockchain.symbol + " " + type;
     
     overlayPanel.toggle(event);
   }

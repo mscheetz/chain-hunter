b48bfda76e13627df7d9595e9c82eb682bbba737
Matt Scheetz <mfscheetz@gmail.com>
2019-12-04 21:02:48 -0500

    clean up

--- routes/admin.api.js
+++ routes/admin.api.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 const adminSvc = require("../services/admin.service");
 const discountCodeSvc = require("../services/discount-code.service");
-const dataSvc = require("../data/dataIntegration.service");
 const apiHelp = require("../services/apihelper.service");
-const enums = require('../classes/enums');
 
 router.get("/api/admin/users", [apiHelp.bootlegMiddleware, apiHelp.authMiddleware, apiHelp.adminMiddleware], async (req, res, next) => {
     const result = await adminSvc.getUserCounts();

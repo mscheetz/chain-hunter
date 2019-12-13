e00af79af06b403f138f69514ee2c3e504bd915a
Matt Scheetz <mfscheetz@gmail.com>
2019-12-12 22:01:37 -0500

    eth block support

--- services/blockchains/eth.js
+++ services/blockchains/eth.js
@@ -3,7 +3,9 @@ const _ = require('lodash');
 const helperSvc = require('../helper.service.js');
 const ethplorerBase = "https://ethplorer.io";
 const ethplorerApiBase = "https://ethplorer.io/service/service.php?data=";
+const etherscanBase = "https://api.etherscan.io/api"
 const enums = require('../../classes/enums');
+const config = require('../../config');
 const delay = time => new Promise(res=>setTimeout(res,time));
 
 const getEmptyBlockchain = async() => {
@@ -22,24 +24,27 @@ const getEmptyBlockchain = async() => {
 const getBlockchain = async(chain, toFind, type) => {
     //const chain = await getEmptyBlockchain(blockchain);
 
-    if(toFind.substr(0,2) === "0x") {
+    //if(toFind.substr(0,2) === "0x") {
         chain.address = null;
+        chain.block = null;
         chain.transaction = null;
         chain.contract = null;
 
         const searchType = type === enums.searchType.nothing 
         ? helperSvc.searchType(chain.symbol.toLowerCase(), toFind)
         : type;
-
+        
         if(searchType & enums.searchType.nothing) {
+        } else if (searchType & enums.searchType.block) {
+            chain.block = await getBlock(toFind);
         } else {
             chain = await ethCheck(chain, toFind);
         }
 
-        if(chain.address || chain.transaction || chain.contract) {
+        if(chain.address || chain.block || chain.transaction || chain.contract) {
             chain.icon = "color/"+ chain.symbol.toLowerCase()  +".png";
         }
-    }
+    //}
     
     return chain;
 }
@@ -276,6 +281,93 @@ const getLatestBlock = async(hash) => {
     } catch (error) {
         return 0;
     }
+}
+
+const getBlock = async(blockNumber) => {
+    const blockHex = helperSvc.numberToHex(blockNumber);
+    
+    let endpoint = `?module=proxy&action=eth_getBlockByNumber&tag=${blockHex}&boolean=true&apikey=${config.ETHERSCAN_API_KEY}`;
+    let url = etherscanBase + endpoint;
+
+    try {
+        const response = await axios.get(url);
+
+        if(typeof response.data.result !== 'undefined') {
+            const datas = response.data.result;
+
+            const ts = helperSvc.hexToNumber(datas.timestamp);
+            const size = helperSvc.hexToNumber(datas.size);
+
+            let block = {
+                blockNumber: blockNumber,
+                validator: datas.miner,
+                transactionCount: datas.transactions.length,
+                date: helperSvc.unixToUTC(ts),
+                size: `${helperSvc.commaBigNumber(size.toString())} bytes`,
+                hash: datas.hash,
+                hasTransactions: true
+            };
+            
+            let transactions = [];
+            if(datas.transactions.length > 0) {
+                let values = [];
+                const lastestBlock = await getLatestBlock();
+                datas.transactions.forEach(tx => {
+                    const transaction = buildTransaction(tx, ts, blockNumber, lastestBlock);
+
+                    if(transaction.tos.length > 0) {
+                        let txnValues = transaction.tos.map(t => +t.quantity);
+                        values = _.concat(values, txnValues);
+                    }
+                    transactions.push(transaction);
+                });
+                if(block.transactionCount === transactions.length) {
+                    let totalVolume = 0;
+                    if(values.length > 0) {
+                        const quantity = values.reduce((a, b) => a + b, 0);
+                        totalVolume = helperSvc.commaBigNumber(quantity.toString());
+                    }
+                    block.volume = totalVolume;
+                }
+                block.transactions = transactions;
+            }
+
+            return block;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        return null;
+    }
+}
+
+const buildTransaction = function(tx, ts, block, lastestBlock) {
+    const quantity = helperSvc.hexToNumber(tx.value);
+    const qty = quantity/1000000000000000000;
+    const symbol = "ETH";
+    let froms = [];
+    let tos = [];
+    const from = helperSvc.getSimpleIO(symbol, tx.from, qty);
+    froms.push(from);
+    const to = helperSvc.getSimpleIO(symbol, tx.to, qty);
+    tos.push(to);
+
+    const fromData = helperSvc.cleanIO(froms);
+    const toData = helperSvc.cleanIO(tos);
+
+    const confirmations = lastestBlock - block;
+
+    let txn = {
+        type: enums.transactionType.TRANSFER,
+        hash: tx.hash,
+        block: block,
+        confirmations: confirmations,
+        date: helperSvc.unixToUTC(ts),
+        froms: fromData,
+        tos: toData
+    };
+
+    return txn;
 }
 
 module.exports = {
